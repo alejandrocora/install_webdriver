00e214abb2a42aedd8e5ca7a7aeca3b5231c4f1e
alejandrocora <alzapicolara@gmail.com>
2022-06-26 21:40:14 +0200

    initiated repository

--- install_webdriver.sh
+++ install_webdriver.sh
@@ -5,23 +5,24 @@ echo "	1) Firefox"
 echo "	2) Chrome"
 echo "	3) All"
 echo "	4) Exit"
-echo -n "	> "
+echo -n "> "
 read target
 
-if [ $target == 4 ]; then
+if [ $target -ge 4 ]; then
  	 exit
 fi
 
 version=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/mozilla/geckodriver/releases/latest | grep -P '[^tag/]*$' -o)
 architecture=$(uname -m | grep -P '[^_]*$'  -o)
-echo $architecture
-echo $firefox_download
 
 if [ $target == 1 ] || [ $target == 3 ]; then
+	echo "	Installing Firefox geckodriver."
 	firefox_download=https://github.com/mozilla/geckodriver/releases/download/$version/geckodriver-$version-linux$architecture.tar.gz
-	echo $firefox_download
+	echo "	Downloading file..."
 	curl -L $firefox_download --output /tmp/geckodriver.tar.gz
+	echo "	Uncompressing..."
 	tar xf /tmp/geckodriver.tar.gz -C /tmp/
+	echo "	Moving to '/usr/bin/'..."
 	mv /tmp/geckodriver /usr/bin/geckodriver
 	rm -rf /tmp/geckodriver.tar.gz
 fi
@@ -29,3 +30,5 @@ fi
 # if [ $target == 2 ] || [ $target == 3 ]; then
 # else
 # fi
+
+echo "	Installation finished."
