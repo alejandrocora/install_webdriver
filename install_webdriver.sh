d6e92807a647b7476830cb42327d2831a62b704d
alejandrocora <alzapicolara@gmail.com>
2022-06-28 17:37:32 +0200

    added chromedriver installation

--- install_webdriver.sh
+++ install_webdriver.sh
@@ -1,34 +1,63 @@
 #!/bin/bash
 
+function dem {
+	echo "	Downloading file..."
+	curl -L $1 --output $2
+	echo "	Uncompressing..."
+	file_term=$(echo $2 | grep -P -o '[.].*')
+	if [ $file_term == '.tar.gz' ]; then
+		driver_file=$(tar xfv $2 -C $(echo $2 | grep -P -o '.+?(?=\/)'))
+	fi
+	if [ $file_term == '.zip' ]; then
+		driver_file=$(echo $2 | sed "s/\/tmp\///g" | sed "s/$file_term//g")
+		echo $driver_file
+		unzip $2 -d $(echo $2 | grep -P -o '.+?(?=\/)')
+	fi
+	echo "	Moving to '/usr/bin/'..."
+	mv /tmp/$driver_file /usr/bin/$driver_file
+	rm -rf $2
+	echo "	Done!"
+}
+
 echo "Select a WebDriver to install/update: "
-echo "	1) Firefox"
-echo "	2) Chrome"
+echo "	1) Firefox Geckodriver"
+echo "	2) ChromeDriver"
 echo "	3) All"
 echo "	4) Exit"
-echo -n "> "
+echo -n "	> "
 read target
 
 if [ $target -ge 4 ]; then
  	 exit
 fi
 
-version=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/mozilla/geckodriver/releases/latest | grep -P '[^tag/]*$' -o)
-architecture=$(uname -m | grep -P '[^_]*$'  -o)
+architecture=$(uname -m | grep -P '[^_]*$' -o)
 
 if [ $target == 1 ] || [ $target == 3 ]; then
-	echo "	Installing Firefox geckodriver."
-	firefox_download=https://github.com/mozilla/geckodriver/releases/download/$version/geckodriver-$version-linux$architecture.tar.gz
-	echo "	Downloading file..."
-	curl -L $firefox_download --output /tmp/geckodriver.tar.gz
-	echo "	Uncompressing..."
-	tar xf /tmp/geckodriver.tar.gz -C /tmp/
-	echo "	Moving to '/usr/bin/'..."
-	mv /tmp/geckodriver /usr/bin/geckodriver
-	rm -rf /tmp/geckodriver.tar.gz
+	echo "	Installing Firefox Geckodriver."
+	firefox_version=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/mozilla/geckodriver/releases/latest | grep -P '[^tag/]*$' -o)
+	firefox_download=https://github.com/mozilla/geckodriver/releases/download/$firefox_version/geckodriver-$firefox_version-linux$architecture.tar.gz
+	dem $firefox_download /tmp/geckodriver.tar.gz
+	echo ""
 fi
 
-# if [ $target == 2 ] || [ $target == 3 ]; then
-# else
-# fi
+if [ $target == 2 ] || [ $target == 3 ]; then
+	echo "	Installing ChromeDriver."
+	chrome_scrap=$(curl https://chromedriver.chromium.org/downloads 2>&1 | grep -P '(?<=please download ChromeDriver ).*$' -o | sort | uniq | tr "\n" " ")
+	IFS=' ' read -ra chrome_versions <<< $chrome_scrap
+	echo "Select a ChromeDriver version: "
+	i=0
+	for element in "${chrome_versions[@]}"
+	do
+		i=$((i+1))
+		echo "	$i) $element"
+	done
+	echo -n "	>"
+	read selection
+	chrome_version=$((selection-1))
+	chrome_download=https://chromedriver.storage.googleapis.com/${chrome_versions[chrome_version]}/chromedriver_linux$architecture.zip
+	dem $chrome_download /tmp/chromedriver.zip
+	echo ""
+fi
 
 echo "	Installation finished."
